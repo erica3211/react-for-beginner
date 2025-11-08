8f143751d68fb53d1443444e8fc91154a26b323a
moonhyejee <erica3211@naver.com>
2025-11-09 01:35:17 +0900

    #10.1 Reusable Components with JSX + Props (09:12) 강의 내용

--- src/app/routes/UrlList.jsx
+++ src/app/routes/UrlList.jsx
@@ -2,7 +2,6 @@ import Home from "../../pages/Home";
 import Coins from "../../pages/Coins";
 import TodoList from "../../pages/ToDoList";
 import UseEffect from "../../pages/UseEffect";
-import Movie from "../../pages/MovieList";
 import MovieDetail from "../../pages/MovieDetail";
 import MovieList from "../../pages/MovieList";
 import Potato from "../../pages/Potato";
@@ -15,8 +14,7 @@ function UrlList() {
     { url: "/coins", page: <Coins />, name: "Coins" },
     { url: "/movieList", page: <MovieList />, name: "MovieList" },
     { url: "/detail/:id", page: <MovieDetail />, name: "MovieDetail" },
-    { url: "/potato/:id", page: <Potato />, name: "Potato" },
-
+    { url: "/potato", page: <Potato />, name: "Potato" },
   ];
 }
 export default UrlList;
