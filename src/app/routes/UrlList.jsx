d5f55d9322e7561681b43cb7e0612bfe72f36eec
moonhyejee <erica3211@naver.com>
2025-11-12 22:35:04 +0900

    #11.0 Class Components and State (10:11) 강의 내용

--- src/app/routes/UrlList.jsx
+++ src/app/routes/UrlList.jsx
@@ -5,6 +5,7 @@ import UseEffect from "../../pages/UseEffect";
 import MovieDetail from "../../pages/MovieDetail";
 import MovieList from "../../pages/MovieList";
 import FoodList from "../../pages/FoodList";
+import State from "../../pages/State";
 
 function UrlList() {
   return [
@@ -15,6 +16,7 @@ function UrlList() {
     { url: "/movieList", page: <MovieList />, name: "MovieList" },
     { url: "/detail/:id", page: <MovieDetail />, name: "MovieDetail" },
     { url: "/foodList", page: <FoodList />, name: "FoodList" },
+    { url: "/state", page: <State />, name: "State" },
   ];
 }
 export default UrlList;
