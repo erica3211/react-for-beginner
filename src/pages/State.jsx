4208fd55616ee11f092641382f8a184aabd8efc9
moonhyejee <erica3211@naver.com>
2025-11-12 23:03:50 +0900

    #11.2 Component Life Cycle (08:07) 강의 내용

--- src/pages/State.jsx
+++ src/pages/State.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import GoBackHome from "../components/GoBackHome";
 
 class State extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("컴포넌트 생성");
+    }
     state = {
         count : 0
     };
@@ -12,9 +16,21 @@ class State extends React.Component{
     minus = () => {
         this.setState(current =>({count: current.count - 1}));
     };
+    componentDidMount(){
+        console.log("컴포넌트 렌더링 완료");
+    };
+    componentDidUpdate(){
+        console.log("컴포넌트 업데이트");
+    };
+    componentWillUnmount(){
+        console.log("컴포넌트 잘가~");
+    }
+
     render(){
+        console.log("렌더링 중");
         return (
             <div>
+                <GoBackHome page={"/"} />
                 <h1>The number is {this.state.count}</h1>
                 <button onClick={this.add}>Add</button>
                 <button onClick={this.minus}>Minus</button>
