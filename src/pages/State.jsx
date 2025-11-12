909ca949112c95c036b4c2a0640cd9125acca536
moonhyejee <erica3211@naver.com>
2025-11-12 23:13:42 +0900

    #11.3 Planning the Movie Component (05:02) 강의 내용

--- src/pages/State.jsx
+++ src/pages/State.jsx
@@ -1,41 +1,22 @@
 import React, { useState } from "react";
 import GoBackHome from "../components/GoBackHome";
 
-class State extends React.Component{
-    constructor(props){
-        super(props);
-        console.log("컴포넌트 생성");
-    }
+class State extends React.Component {
     state = {
-        count : 0
-    };
-    add = () => {
-        this.setState(current =>({count: current.count + 1}));
-
-    };
-    minus = () => {
-        this.setState(current =>({count: current.count - 1}));
-    };
-    componentDidMount(){
-        console.log("컴포넌트 렌더링 완료");
-    };
-    componentDidUpdate(){
-        console.log("컴포넌트 업데이트");
-    };
-    componentWillUnmount(){
-        console.log("컴포넌트 잘가~");
+        isLoading: true
     }
-
-    render(){
-        console.log("렌더링 중");
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, 6000);
+    }
+    render() {
+        const { isLoading } = this.state;
         return (
             <div>
-                <GoBackHome page={"/"} />
-                <h1>The number is {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
+                {isLoading ? "Loading..." : "We are ready"}
             </div>
-        )
+        );
     }
 }
 
